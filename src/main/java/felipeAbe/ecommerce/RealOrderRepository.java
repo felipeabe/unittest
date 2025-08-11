01dc82a9cd3f144adc8957e570d4f81d4da15147
felipeabe <felipehabe301@gmail.com>
2025-08-11 11:36:00 -0300

    Initial commit

--- src/main/java/felipeAbe/ecommerce/RealOrderRepository.java
+++ src/main/java/felipeAbe/ecommerce/RealOrderRepository.java
@@ -5,7 +5,11 @@ import java.util.Map;
 
 // Implementação real do repositório de pedidos
 public class RealOrderRepository implements OrderRepository {
-    private Map<Integer, Order> orders = new HashMap<>();
+    private Map<Integer, Order> orders;
+
+    public RealOrderRepository(Map<Integer, Order> orders) {
+        this.orders = orders;
+    }
 
     @Override
     public void save(Order order) {
--- src/test/java/felipeAbe/ecommerce/RealOrderRepositoryTest.java
+++ src/test/java/felipeAbe/ecommerce/RealOrderRepositoryTest.java
@@ -1,23 +1,44 @@
 package felipeAbe.ecommerce;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 
 
 class RealOrderRepositoryTest {
 
-    private RealOrderRepository realOrderRepository=new RealOrderRepository();
+    @Spy
+    private HashMap<Integer,Order> ordersSpy;
+
+    @InjectMocks
+    private RealOrderRepository realOrderRepository;
 
     @Nested
     class save{
 
         @Test
-        void
+        @DisplayName("Should save order")
+        void shouldSaveOrder(){
 
+            //arrange
+            var dummyOrder=new Order(1,"Bruno", 200);
+            assertNull(realOrderRepository.findById(dummyOrder.getId()));
+
+            //act
+            realOrderRepository.save(dummyOrder);
+
+            //assert
+            verify(ordersSpy)
+        }
     }
 
 
