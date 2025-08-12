7c103c0d75447522c0ad7d6f64a653757aef4f4a
felipe <felipehabe301@gmail.com>
2025-08-12 10:31:40 -0300

    testes parametrizados

---
+++ src/test/java/felipeAbe/ecommerce/OrderServiceTest.java
@@ -0,0 +1,119 @@
+package felipeAbe.ecommerce;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class OrderServiceTest {
+
+    @Mock
+    private OrderRepository repository;
+
+    @Captor
+    private ArgumentCaptor<Order> orderArgumentCaptor;
+
+    @InjectMocks
+    private OrderService orderService;
+
+    @Nested
+    class placeOrder {
+
+        @Test
+        @DisplayName("place order when total is greater than zero")
+        void shouldPlaceAnOrderWhenTotalIsGreaterThanZero(){
+
+            //arrange
+            Order dummyOrder=new Order(1,"random",5);
+
+            //act
+            orderService.placeOrder(dummyOrder);
+
+            //asert
+            verify(repository, times(1)).save(orderArgumentCaptor.capture());
+            var orderCaptured=orderArgumentCaptor.getValue();
+            assertSame(dummyOrder, orderCaptured);
+        }
+
+        @ParameterizedTest
+        @ValueSource(doubles = {0, -2.0, -50.0})
+        @DisplayName("place order when total is below or equal to zero")
+        void shouldThrowExceptionWhenTotalIsZero(double total){
+
+            //arrange
+            var dummyOrder=new Order(1,"random",total);
+
+            //act
+            assertThrows(IllegalArgumentException.class, ()->{
+                orderService.placeOrder(dummyOrder);
+            });
+
+            //asert
+            verify(repository, times(0)).save(any());
+        }
+
+        @Test
+        @DisplayName("place order when total is greater than zero")
+        void shouldPlaceAnOrderWhenTotalIsGreaterThanZero(){
+
+            //arrange
+            Order dummyOrder=new Order(1,"random",5);
+
+            //act
+            orderService.placeOrder(dummyOrder);
+
+            //asert
+            verify(repository, times(1)).save(orderArgumentCaptor.capture());
+            var orderCaptured=orderArgumentCaptor.getValue();
+            assertSame(dummyOrder, orderCaptured);
+        }
+    }
+
+    @Nested
+    class getOrder{
+
+        @Test
+        @DisplayName("Should return order when exists")
+        void shouldGReturnOrderWhenExists(){
+
+            //arrange
+            int idOrder=1;
+            Order dummyOrder=new Order(idOrder,"random",5);
+            doReturn(dummyOrder).when(repository).findById(eq(idOrder));
+
+            //act
+            var output=orderService.getOrder(idOrder);
+
+            //assert
+            assertNotNull(output);
+            assertSame(dummyOrder, output);
+            verify(repository, times(1)).findById(eq(idOrder));
+        }
+
+        @Test
+        @DisplayName("Should return null when order doesn't exists")
+        void shouldGReturnNullWhenOrderDOesntExists(){
+
+            //arrange
+            int idOrder=1;
+            doReturn(null).when(repository).findById(eq(idOrder));
+
+            //act
+            var output=orderService.getOrder(idOrder);
+
+            //assert
+            assertNull(output);
+            verify(repository, times(1)).findById(eq(idOrder));
+        }
+    }
+
+}
--- src/test/java/felipeAbe/ecommerce/RealOrderRepositoryTest.java
+++ src/test/java/felipeAbe/ecommerce/RealOrderRepositoryTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -11,37 +13,87 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
-
+@ExtendWith(MockitoExtension.class)
 class RealOrderRepositoryTest {
 
     @Spy
-    private HashMap<Integer,Order> ordersSpy;
+    private HashMap<Integer, Order> ordersSpy;
+
+    @Captor
+    private ArgumentCaptor<Order> orderArgumentCaptor;
 
     @InjectMocks
     private RealOrderRepository realOrderRepository;
 
     @Nested
-    class save{
+    class save {
 
         @Test
         @DisplayName("Should save order")
-        void shouldSaveOrder(){
+        void shouldSaveOrder() {
 
             //arrange
-            var dummyOrder=new Order(1,"Bruno", 200);
-            assertNull(realOrderRepository.findById(dummyOrder.getId()));
+            var dummyOrder = new Order(1, "Bruno", 200);
 
             //act
             realOrderRepository.save(dummyOrder);
 
             //assert
-            verify(ordersSpy)
+            verify(ordersSpy, times(1)).put(eq(dummyOrder.getId()), orderArgumentCaptor.capture());
+            var orderCaptured = orderArgumentCaptor.getValue();
+            assertSame(dummyOrder, orderCaptured);
         }
     }
 
+    @Nested
+    class findById {
+
+        @Test
+        @DisplayName("Should find by id")
+        void shouldFindById() {
+
+            //arrange
+            var id = 1;
+            var dummyOrder = new Order(1, "Bruno", 200);
+            doReturn(dummyOrder).when(ordersSpy).get(eq(id));
+
+            //act
+            var order = realOrderRepository.findById(id);
+
+            //assert
+            assertNotNull(order);
+            assertSame(dummyOrder, order);
+            verify(ordersSpy, times(1)).get(eq(id));
+
+        }
 
 
+        @Test
+        @DisplayName("Should return null when order doesn't exists")
+        void shouldFindByIdWhenOrderDoesntExists() {
 
+            //arrange
+            var id = 1;
+            doReturn(null).when(ordersSpy).get(eq(id));
+
+            //act
+            var order = realOrderRepository.findById(id);
+
+            //assert
+            assertNull(order);
+            verify(ordersSpy, times(1)).get(eq(id));
+
+        }
+
+    }
 }
+
+
+
+
+
+
